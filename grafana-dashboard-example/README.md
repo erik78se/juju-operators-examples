d79262072f57960e87699eb4c52f6480be36c442
erik78se <erik.lonroth@gmail.com>
2022-04-12 20:52:51 +0200

    Working, but needs to get rid of a duplicate dashboard

--- grafana-dashboard-example/README.md
+++ grafana-dashboard-example/README.md
@@ -1,7 +1,7 @@
-# monitoring-nrpe
-A simple charm that shows how to implement some monitoring with charmhelpers and nrpe to use with nagios.
+# grafana-dashboard-example
 
-Uses charmhelpers lib to work out the nrpe bits. See: https://github.com/juju/charm-helpers
+This juju charm implements the [grafana-dashboard interface] to send over a custom dashboard to grafana using the [prometheus2 charm] to connect it with a datasource.
+
 
 ## Usage
 
@@ -10,42 +10,73 @@ Uses charmhelpers lib to work out the nrpe bits. See: https://github.com/juju/ch
     juju model-config default-series=focal
     juju model-config logging-config="<root>=WARNING;unit=TRACE"
 
+    # Deploy grafana
     juju deploy grafana
+    
+    # Deploy prometheus2 as prometheus since this seems to help.
+    juju deploy prometheus2 prometheus
+
+    # Deploy our charm
     juju deploy ./grafana-dashboard-example_ubuntu-20.04-amd64.charm
-    juju relate grafana grafana-dashboard-example
+
+    # Connect with prometheus and grafana
+    juju relate grafana-dashboard-example:scrape prometheus
+    juju relate grafana-dashboard-example:grafana-dashboard grafana
 
     # Visit grafana http://grafana:3000
     # Get admin password:
     juju run-action grafana/0 get-login-info --wait
 
 ## General implementation details
-1. Update metadata.yaml
 
-       provides:
-         hello-dashboard:
-         interface: grafana-dashboard
+We provide two relations in [metadata.yaml]():
 
-3. Add custom dashboards to 
+    provides:
+      grafana-dashboard:
+        interface: grafana-dashboard
+      scrape:
+        interface: prometheus
 
-       files/grafana-dashboards/foobar.json
+We implement those in [src/charm.py]()
 
-4. Implement the code to send the dashboard when the relation is joined.
+In the install event, prometheus-node-exporter is installed that collects and provides metrics. This is related with prometheus which create a datasource for us.
 
-       charm.py
+### The dashboard-template.json should contain:
+```
+   "__inputs": [
+     {
+       "name": "DS_INFRA",
+       "label": "infra",
+       "description": "",
+       "type": "datasource",
+       "pluginId": "prometheus",
+       "pluginName": "Prometheus"
+     }
+   ], 
+```
+The placeholders (DS_INFRA) is replaced in the code, with the datasource name which we modify before  ending them over to grafana with the relation data.
 
-5. Update charmcraft.yaml to pass along the files
+## Your own dashboard
 
-       parts:
-        charm:
-        charm-python-packages: [setuptools < 58]
-        prime:
-          - files/*
+If you intend to roll your own dashboard to send to grafana, you need to properly export it using "share for export externally" which brings along a usable json. You still need to edit it to fit your scenario.
 
-6. Add to requirements.txt
-
-       ops >= 1.4.0
-
+![Alt text](share-export-dashboard.png?raw=true "Exporting a dashboard")
 
 ## Authors
-Erik Lönroth, support me by attributing my work
+Erik Lönroth, support me by attributing my work and pay me.
 https://eriklonroth.com
+
+
+## Attibutions
+
+OpenStack charmers team for code: https://https://opendev.org/openstack/charm-ceph-dashboard/
+
+LMA charmers team for prometheus2 charm: https://git.launchpad.net/charm-prometheus2
+
+Juju team for the grafana-dashboard interface: https://github.com/juju-solutions/interface-grafana-dashboard
+
+LXD charm developers for a massive example: https://github.com/canonical/charm-lxd/
+
+
+[prometheus2 charm]: https://git.launchpad.net/charm-prometheus2
+[grafana-dashboard interface]: https://github.com/juju-solutions/interface-grafana-dashboard
--- grafana-dashboard-example/src/charm.py
+++ grafana-dashboard-example/src/charm.py
@@ -6,8 +6,11 @@ import copy
 import os
 import logging
 import json
+import time
 from pathlib import Path
-from ops.charm import CharmBase
+import socket
+from typing import List
+from ops.charm import CharmBase, RelationChangedEvent, RelationDepartedEvent
 from ops.main import main
 from ops.model import ActiveStatus
 
@@ -29,37 +32,133 @@ class GrafanaDashBoardCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
 
-        self.framework.observe(self.on.hello_dashboard_relation_joined,
-                               self._send_dashboard)
-
-        # self.framework.observe(self.on.hello_dashboard_relation_changed,
-        #                       self._send_dashboard)
-
-    def _send_dashboard(self, event):
-        """
-        Loads json from file, converts it to string and places it on the relation.
         
+        self.framework.observe(self.on.install,
+                                self._install)
+
+        # The dashboard relation - provides the dashboard
+        self.framework.observe(self.on.grafana_dashboard_relation_changed,
+                                self._on_grafana_dashboard_relation_changed)
+
+        # The prometheus scrape endpoint - provides some data for our dashboard via prometheus
+        self.framework.observe(self.on.scrape_relation_changed, 
+                                self._on_scrape_relation_changed)
+        
+    def _install(self,event):
         """
-        dashboard_file = Path('files/grafana-dashboards/hello-world.json')
+        Install prometheus-node-exporter so to provide some metrics.
+        """
+        logger.debug("Installing prometheus-node-exporter, test with curl -L localhost:9100/metrics")
+        os.system('apt -y install prometheus-node-exporter')
+        
 
-        # Load up json
-        f = open(dashboard_file)
-        _dashboard = json.load(f)
-        f.close()
 
-        # We add in a source_model key,value
-        _dashboard["source_model"] = self.model.name
+    def _on_grafana_dashboard_relation_changed(self, event: RelationChangedEvent) -> None:
+        """Provide the dashboard to Grafana."""
+        # Only one dashboard is needed so let the app leader deal with it
+        if not self.unit.is_leader():
+            return
 
-        logger.debug("Sending dashboard: " + json.dumps(_dashboard, indent=4, sort_keys=True))
+        # Check if there is an existing relation named "prometheus-manual"
+        scrape_relation = self.model.get_relation("scrape")
+        if not scrape_relation:
+            logger.error(
+                "Missing scrape relation required by grafana-dashboard relation"
+            )
+            return
 
-        # Set data on the event relation.
-        # event.relation.data[self.model.unit]['dashboard'] = json.loads(dashboard_file.read_text())
-        event.relation.data[self.model.unit]['dashboard'] = json.dumps(_dashboard)
-        event.relation.data[self.model.unit]['name'] = "myName1"
+        if not scrape_relation.app.name:
+            logger.error("Missing app.name for scrape relation")
+            return
 
-        # Done
-        self.unit.status = ActiveStatus("Dashboard sent.")
-    
+        # Load the dashboard which contains some labels we will replace with
+        # reference to datasources we provide from the scrape relation.
+        dashboard_file = "files/grafana-dashboards/dashboard-template.json"
+        if not os.path.exists(dashboard_file):
+            logger.error("No dashboard for grafana was bundled in the charm")
+            return
+
+        with open(dashboard_file) as f:
+            data = f.read()
+            dashboard = json.loads(data)
+
+        # The bundled dashboard should contain:
+        #   "__inputs": [
+        #     {
+        #       "name": "DS_INFRA",
+        #       "label": "infra",
+        #       "description": "",
+        #       "type": "datasource",
+        #       "pluginId": "prometheus",
+        #       "pluginName": "Prometheus"
+        #     }
+        #   ],
+        # and the name value needs to be replaced by the proper datasource name which
+        # is derived from the application name used when Prometheus2 was deployed.
+        ds_prometheus = f'"{scrape_relation.app.name} - Juju generated source"'
+
+        # Safety checks
+        if "__inputs" not in dashboard or len(dashboard["__inputs"]) != 1:
+            logger.error(f'{dashboard_file} has invalid or missing "__inputs" section')
+            return
+
+        # Get the name of the datasource that will need to be replaced
+        ds_to_replace = dashboard["__inputs"][0].get("name")
+        if not ds_to_replace:
+            logger.error(f"{dashboard_file} is malformed")
+            return
+
+        # Replace the datasource name
+        #   "name": "DS_INFRA"   -> "name": "ds_prometheus"
+        #   "uid": "${DS_INFRA}" -> "uid": "ds_prometheus"
+        data = data.replace('"' + ds_to_replace + '"', ds_prometheus).replace(
+            '"${' + ds_to_replace + '}"', ds_prometheus
+        )
+
+        # Reload the mangled data as JSON
+        dashboard = json.loads(data)
+
+        # XXX: Introduce an artificial delay before sending the dashboard to
+        # Grafana to give time for the prometheus2:grafana-source relation
+        # to be established. Without that, the injected dashboard shows
+        # as empty requiring manual intervention:
+        # # > remove the relation
+        # juju remove-relation grafana-dashboard-example:grafana-dashboard grafana:dashboards
+        # # > log to grafana using the admin password obtained with:
+        # juju run-action --wait grafana/leader get-admin-password
+        # # > delete the bogus dashboard
+        # # > recreate the relation
+        # juju add-relation grafana-dashboard-example:grafana-dashboard grafana:dashboards
+
+        # TODO: Not sure why we need to do this.
+
+        time.sleep(60)
+
+        # Send a compact JSON version of the dashboard to Grafana
+        event.relation.data[self.app].update(
+            {
+                "name": self.app.name.upper(),
+                "dashboard": json.dumps(
+                    dashboard,
+                    separators=(",", ":"),
+                ),
+            }
+        )
+        logger.debug("Dashboard sent to Grafana")
+
+    def _on_scrape_relation_changed(self, event: RelationChangedEvent) -> None:
+        """
+        Provide basic node exporter metrics
+        """
+        _port = 9100
+        _hostname = socket.getfqdn()
+        _metrics_path = '/metrics'
+
+        event.relation.data[self.model.unit]['hostname'] = str(_hostname)
+        event.relation.data[self.model.unit]['port'] = str(_port)
+        event.relation.data[self.model.unit]['metrics_path'] = str(_metrics_path)
+
+
 
 if __name__ == "__main__":
     main(GrafanaDashBoardCharm)
